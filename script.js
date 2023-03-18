7a21bb80d6c770c26557feb513f1897c00e3761a
iminfinity <iminfinity@pm.me>
2023-03-18 19:09:13 +0530

    make javascript undertand complex numbers

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 var totalWidth = 1900;
 var totalHeight = 1000;
+var addComplexNumbers = function (x, y) {
+    var sum = {
+        real: x.real + y.real,
+        coefficient: x.coefficient + y.coefficient,
+    };
+    return sum;
+};
+var squareComplexNumber = function (x) {
+    var square = {
+        real: Math.pow(x.real, 2) - Math.pow(x.coefficient, 2),
+        coefficient: 2 * x.real * x.coefficient,
+    };
+    return square;
+};
 var fc = function (z, c) {
     return Math.pow(z, 2) + c;
 };
@@ -19,4 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
     draw(ctx);
     ctx.fillStyle = "red";
     ctx.fillRect(0, 0, 100, 20);
+    console.log(addComplexNumbers({ real: 5, coefficient: 3 }, { real: 4, coefficient: 2 }));
+    console.log(addComplexNumbers({ real: 3, coefficient: 1 }, { real: -1, coefficient: 2 }));
+    console.log(squareComplexNumber({ real: 1, coefficient: 1 }));
 });
--- script.ts
+++ script.ts
@@ -1,6 +1,27 @@
 const totalWidth = 1900;
 const totalHeight = 1000;
 
+type ComplexNumber = {
+  real: number;
+  coefficient: number;
+};
+
+const addComplexNumbers = (x: ComplexNumber, y: ComplexNumber) => {
+  const sum: ComplexNumber = {
+    real: x.real + y.real,
+    coefficient: x.coefficient + y.coefficient,
+  };
+  return sum;
+};
+
+const squareComplexNumber = (x: ComplexNumber) => {
+  const square: ComplexNumber = {
+    real: Math.pow(x.real, 2) - Math.pow(x.coefficient, 2),
+    coefficient: 2 * x.real * x.coefficient,
+  };
+  return square;
+};
+
 const fc = (z: number, c: number) => {
   return Math.pow(z, 2) + c;
 };
@@ -27,4 +48,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
   ctx.fillStyle = "red";
   ctx.fillRect(0, 0, 100, 20);
+
+  console.log(
+    addComplexNumbers({ real: 5, coefficient: 3 }, { real: 4, coefficient: 2 })
+  );
+  console.log(
+    addComplexNumbers({ real: 3, coefficient: 1 }, { real: -1, coefficient: 2 })
+  );
+  console.log(squareComplexNumber({ real: 1, coefficient: 1 }));
 });
