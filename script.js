e9532a3569618bb36e60e0b2ff14a13faf8cf909
iminfinity <iminfinity@pm.me>
2023-03-19 21:42:29 +0530

    increase canvas size, change palette

--- script.js
+++ script.js
@@ -17,64 +17,25 @@ const fc = (z, c) => {
     return addComplexNumbers(squareComplexNumber(z), c);
 };
 const getColor = (iterations) => {
-    // color palettes from
-    // https://coolors.co/palette/f8f9fa-e9ecef-dee2e6-ced4da-adb5bd-6c757d-495057-343a40-212529
-    // https://coolors.co/palette/03045e-023e8a-0077b6-0096c7-00b4d8-48cae4-90e0ef-ade8f4-caf0f8
-    if (iterations > 999) {
-        return "#000000";
-    }
-    if (iterations > 950) {
-        return "#212529";
-    }
-    if (iterations > 900) {
-        return "#343A40";
-    }
-    if (iterations > 800) {
-        return "#495057";
-    }
-    if (iterations > 700) {
-        return "#6C757D";
-    }
-    if (iterations > 600) {
-        return "#ADB5BD";
-    }
-    if (iterations > 500) {
-        return "#CED4DA";
-    }
-    if (iterations > 400) {
-        return "#DEE2E6";
-    }
-    if (iterations > 300) {
-        return "#E9ECEF";
-    }
-    if (iterations > 200) {
-        return "#F8F9FA";
-    }
-    if (iterations > 100) {
-        return "#03045E";
-    }
-    if (iterations > 70) {
-        return "#023E8A";
-    }
-    if (iterations > 60) {
-        return "#0077B6";
+    if (iterations > 80) {
+        return "purple";
     }
     if (iterations > 50) {
-        return "#0096C7";
+        return "blue";
     }
-    if (iterations > 80) {
-        return "#00B4D8";
+    if (iterations > 20) {
+        return "green";
     }
-    if (iterations > 60) {
-        return "#48CAE4";
+    if (iterations > 5) {
+        return "yellow";
     }
-    if (iterations > 30) {
-        return "#90E0EF";
+    if (iterations > 3) {
+        return "pink";
     }
-    if (iterations > 10) {
-        return "#ADE8F4";
+    if (iterations > 2) {
+        return "red";
     }
-    return "#CAF0F8";
+    return "#caf0f8";
 };
 const getMultiplier = (quadrant) => {
     switch (quadrant) {
@@ -95,7 +56,7 @@ const draw = (ctx) => {
         real: 0,
         coefficient: 0,
     };
-    for (let i = 0; i < 2000; i++) {
+    for (let i = 0; i < 3000; i++) {
         for (let j = 0; j < 2000; j++) {
             let currentIterations = 0;
             let next = fc(z, {
@@ -112,14 +73,16 @@ const draw = (ctx) => {
                 });
                 currentIterations++;
             }
-            drawPixel(ctx, currentIterations, 1, i, j);
+            if (currentIterations < 200) {
+                drawPixel(ctx, currentIterations, 1, i, j);
+            }
             currentIterations = 0;
             next = fc(z, {
                 real: (x4 * i) / 1000,
                 coefficient: (y4 * j) / 1000,
             });
             while (currentIterations < 1000) {
-                if (Math.pow(next.real * next.coefficient, 2) > 50) {
+                if (Math.pow(next.real * next.coefficient, 2) > 5000) {
                     break;
                 }
                 next = fc(next, {
@@ -128,7 +91,9 @@ const draw = (ctx) => {
                 });
                 currentIterations++;
             }
-            drawPixel(ctx, currentIterations, 4, i, j);
+            if (currentIterations < 200) {
+                drawPixel(ctx, currentIterations, 4, i, j);
+            }
         }
     }
 };
@@ -148,10 +113,12 @@ const drawPixel = (ctx, iterations, quadrant, x, y) => {
 document.addEventListener("DOMContentLoaded", () => {
     const app = document.getElementById("app");
     app.height = 3000;
-    app.width = 4000;
+    app.width = 5000;
     const ctx = app.getContext("2d");
     if (!ctx)
         return;
+    ctx.fillStyle = "#101010";
+    ctx.fillRect(0, 0, app.width, app.height);
     ctx.translate(3000, 1500);
     draw(ctx);
 });
--- script.ts
+++ script.ts
@@ -26,65 +26,25 @@ const fc = (z: ComplexNumber, c: ComplexNumber) => {
 };
 
 const getColor = (iterations: number) => {
-  // color palettes from
-  // https://coolors.co/palette/f8f9fa-e9ecef-dee2e6-ced4da-adb5bd-6c757d-495057-343a40-212529
-  // https://coolors.co/palette/03045e-023e8a-0077b6-0096c7-00b4d8-48cae4-90e0ef-ade8f4-caf0f8
-  if (iterations > 999) {
-    return "#000000";
-  }
-  if (iterations > 950) {
-    return "#212529";
-  }
-  if (iterations > 900) {
-    return "#343A40";
-  }
-  if (iterations > 800) {
-    return "#495057";
-  }
-  if (iterations > 700) {
-    return "#6C757D";
-  }
-  if (iterations > 600) {
-    return "#ADB5BD";
-  }
-  if (iterations > 500) {
-    return "#CED4DA";
-  }
-  if (iterations > 400) {
-    return "#DEE2E6";
-  }
-  if (iterations > 300) {
-    return "#E9ECEF";
-  }
-  if (iterations > 200) {
-    return "#F8F9FA";
-  }
-  if (iterations > 100) {
-    return "#03045E";
-  }
-  if (iterations > 70) {
-    return "#023E8A";
-  }
-  if (iterations > 60) {
-    return "#0077B6";
+  if (iterations > 80) {
+    return "purple";
   }
   if (iterations > 50) {
-    return "#0096C7";
+    return "blue";
   }
-  if (iterations > 80) {
-    return "#00B4D8";
+  if (iterations > 20) {
+    return "green";
   }
-  if (iterations > 60) {
-    return "#48CAE4";
+  if (iterations > 5) {
+    return "yellow";
   }
-  if (iterations > 30) {
-    return "#90E0EF";
+  if (iterations > 3) {
+    return "pink";
   }
-  if (iterations > 10) {
-    return "#ADE8F4";
+  if (iterations > 2) {
+    return "red";
   }
-
-  return "#CAF0F8";
+  return "#caf0f8";
 };
 
 const getMultiplier = (quadrant: Quadrant) => {
@@ -109,7 +69,7 @@ const draw = (ctx: CanvasRenderingContext2D) => {
     coefficient: 0,
   };
 
-  for (let i = 0; i < 2000; i++) {
+  for (let i = 0; i < 3000; i++) {
     for (let j = 0; j < 2000; j++) {
       let currentIterations = 0;
 
@@ -131,7 +91,9 @@ const draw = (ctx: CanvasRenderingContext2D) => {
         currentIterations++;
       }
 
-      drawPixel(ctx, currentIterations, 1, i, j);
+      if (currentIterations < 200) {
+        drawPixel(ctx, currentIterations, 1, i, j);
+      }
       currentIterations = 0;
       next = fc(z, {
         real: (x4 * i) / 1000,
@@ -139,7 +101,7 @@ const draw = (ctx: CanvasRenderingContext2D) => {
       });
 
       while (currentIterations < 1000) {
-        if (Math.pow(next.real * next.coefficient, 2) > 50) {
+        if (Math.pow(next.real * next.coefficient, 2) > 5000) {
           break;
         }
 
@@ -150,7 +112,9 @@ const draw = (ctx: CanvasRenderingContext2D) => {
 
         currentIterations++;
       }
-      drawPixel(ctx, currentIterations, 4, i, j);
+      if (currentIterations < 200) {
+        drawPixel(ctx, currentIterations, 4, i, j);
+      }
     }
   }
 };
@@ -178,11 +142,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const app = document.getElementById("app") as HTMLCanvasElement;
 
   app.height = 3000;
-  app.width = 4000;
+  app.width = 5000;
 
   const ctx = app.getContext("2d");
 
   if (!ctx) return;
+
+  ctx.fillStyle = "#101010";
+  ctx.fillRect(0, 0, app.width, app.height);
 
   ctx.translate(3000, 1500);
 
